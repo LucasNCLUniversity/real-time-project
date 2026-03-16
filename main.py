5c00bc8252caa2e2c2e08e6ab233db3a24991721
LucasNCLUniversity <l.a.r.bricknell1@newcastle.ac.uk>
2026-03-16 03:12:50 +0000

    added function to get year, month, day, hour, minute, second into a list type

---
+++ main.py
@@ -0,0 +1,13 @@
+#imports
+import datetime
+import re #for splitting the string
+
+#this function splits datetime.now() into seperate values (day, month, year, time)
+def dateSplit():
+    delimiters = '-', ' ', ':'
+    current = str(datetime.datetime.now())
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    listedTime = re.split(regexPattern, current)
+    return listedTime
+
+dateSplit()
