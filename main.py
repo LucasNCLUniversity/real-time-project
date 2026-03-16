b5e308f1e23cd0bb67e2e5e3b5df6bdb83e8641b
LucasNCLUniversity <l.a.r.bricknell1@newcastle.ac.uk>
2026-03-16 21:14:09 +0000

    Removed unncessary python logic (almost all logic is now javascript nested inside index.html)

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 #imports
 from flask import Flask, render_template #for the frontend
-import datetime
-import re #for splitting the string
-
-#this function splits datetime.now() into seperate values (day, month, year, time)
-def dateSplit():
-    delimiters = '-', ' ', ':'
-    current = str(datetime.datetime.now())
-    regexPattern = '|'.join(map(re.escape, delimiters))
-    listedTime = re.split(regexPattern, current)
-    return listedTime #order of list goes [year, month, day, hour, minute, second]
 
 #flask logic for setting up index
 app = Flask('testapp')
 @app.route('/')
 def index():
-    return render_template('index.html', time=dateSplit())
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
